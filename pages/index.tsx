e97b7f0e9cdd8c9d07ad83254d563d251b8dbe37
jessicazu <jessicazu926@gmail.com>
2021-08-01 18:38:35 +0900

    signin用画面作成

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import { signIn, signOut, useSession } from "next-auth/client";
+
+export default function Home() {
+  const [session, loading] = useSession();
+  console.log(session);
+  
+  return (
+    <>
+      {!session && (
+        <>
+          サインインしてください。 <br />
+          <button onClick={signIn}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          サインイン完了。 email: {session.user.email} <br />
+          <button onClick={signOut}>Sign out</button>
+        </>
+      )}
+    </>
+  );
+}
